afec458ce0b615f21f795326549c97c2ca3c98ae
vkaff <vkaffes@imis.athena-innovation.gr>
2020-04-27 21:37:40 +0300

    Tidying up code.

--- interlinking/features.py
+++ interlinking/features.py
@@ -6,11 +6,9 @@ import pandas as pd
 import numpy as np
 from interlinking import config
 from itertools import chain
-import os
-import glob
 
-from interlinking.helpers import transform, getBasePath
-from interlinking.sim_measures import LGMSimVars, lgm_sim_split, score_per_term, weighted_sim, sim_measures
+from interlinking.helpers import transform, StaticValues
+from interlinking import sim_measures # import LGMSimVars, lgm_sim_split, score_per_term, weighted_sim, sim_measures
 
 
 class Features:
@@ -26,19 +24,17 @@ class Features:
     --------
     :func:`compute_features`: Details on the metrics each classification group implements.
     """
-    max_freq_terms = 200
-
-    fields = [
-        "s1",
-        "s2",
-        "status",
-        "gid1",
-        "gid2",
-        "alphabet1",
-        "alphabet2",
-        "alpha2_cc1",
-        "alpha2_cc2",
-    ]
+    # fields = [
+    #     "s1",
+    #     "s2",
+    #     "status",
+    #     "gid1",
+    #     "gid2",
+    #     "alphabet1",
+    #     "alphabet2",
+    #     "alpha2_cc1",
+    #     "alpha2_cc2",
+    # ]
 
     dtypes = {
         's1': str, 's2': str,
@@ -60,7 +56,7 @@ class Features:
     def load_data(self, fname, encoding):
         self.data_df = pd.read_csv(fname, sep=config.delimiter, names=config.fieldnames, dtype=self.dtypes,
                                    na_filter=False, encoding='utf8')
-        LGMSimVars().load_freq_terms(encoding)
+        sim_measures.LGMSimVars().load_freq_terms(encoding)
 
     def build(self):
         """Build features depending on the assignment of parameter :py:attr:`~interlinking.config.MLConf.classification_method`
@@ -126,50 +122,49 @@ class Features:
         """
         f = []
         for status in list({False, sorted}):
+            sim_group = 'basic' if status == False else 'sorted'
+
             a, b = transform(s1, s2, sorting=status, canonical=status)
 
-            sim1 = sim_measures['damerau_levenshtein'](a, b)
-            sim8 = sim_measures['jaccard'](a, b)
-            sim2 = sim_measures['jaro'](a, b)
-            sim3 = sim_measures['jaro_winkler'](a, b)
-            sim4 = sim_measures['jaro_winkler'](a[::-1], b[::-1])
-            sim11 = sim_measures['monge_elkan'](a, b)
-            sim7 = sim_measures['cosine'](a, b)
-            sim9 = sim_measures['strike_a_match'](a, b)
-            sim12 = sim_measures['soft_jaccard'](a, b)
-            if not status: sim5 = sim_measures['sorted_winkler'](a, b)
-            sim10 = sim_measures['skipgram'](a, b)
-            sim13 = sim_measures['davies'](a, b)
-            if status:
-                sim14 = sim_measures['tuned_jaro_winkler'](a, b)
-                sim15 = sim_measures['tuned_jaro_winkler'](a[::-1], b[::-1])
-
-            if status: f.append([sim1, sim2, sim3, sim4, sim7, sim8, sim9, sim10, sim11, sim12, sim13, sim14, sim15])
-            else: f.append([sim1, sim2, sim3, sim4, sim5, sim7, sim8, sim9, sim10, sim11, sim12, sim13])
+            for sim, val in StaticValues.sim_metrics.items():
+                if sim_group in val:
+                    if '_reversed' in sim:
+                        f.append(getattr(sim_measures, sim[:-len('_reversed')])(a[::-1], b[::-1]))
+                    else:
+                        f.append(getattr(sim_measures, sim)(a, b))
 
         if lgm_sims:
+            sim_group = 'lgm'
             a, b = transform(s1, s2, sorting=True, canonical=True)
 
-            sim1 = self._compute_lgm_sim(a, b, 'damerau_levenshtein')
-            sim2 = self._compute_lgm_sim(a, b, 'davies')
-            sim3 = self._compute_lgm_sim(a, b, 'skipgram')
-            sim4 = self._compute_lgm_sim(a, b, 'soft_jaccard')
-            sim5 = self._compute_lgm_sim(a, b, 'strike_a_match')
-            sim6 = self._compute_lgm_sim(a, b, 'cosine')
-            sim7 = self._compute_lgm_sim(a, b, 'jaccard')
-            sim8 = self._compute_lgm_sim(a, b, 'monge_elkan')
-            sim9 = self._compute_lgm_sim(a, b, 'jaro_winkler')
-            sim10 = self._compute_lgm_sim(a, b, 'jaro')
-            sim11 = self._compute_lgm_sim(a, b, 'jaro_winkler_r')
-            sim12 = self._compute_lgm_sim(a, b, 'tuned_jaro_winkler')
-            sim13 = self._compute_lgm_sim(a, b, 'tuned_jaro_winkler_r')
-            sim14, sim15, sim16 = self._compute_lgm_sim_base_scores(a, b, 'damerau_levenshtein')
+            for sim, val in StaticValues.sim_metrics.items():
+                if sim_group in val:
+                    if '_reversed' in sim:
+                        f.append(self._compute_lgm_sim(a[::-1], b[::-1], sim[:-len('_reversed')]))
+                    else:
+                        f.append(self._compute_lgm_sim(a, b, sim))
+            f.extend(list(self._compute_lgm_sim_base_scores(a, b, 'damerau_levenshtein')))
 
-            f.append([
-                sim1, sim2, sim3, sim4, sim5, sim6, sim7, sim8, sim9, sim10, sim11, sim12, sim13, sim14, sim15, sim16
-            ])
+            # sim1 = self._compute_lgm_sim(a, b, 'damerau_levenshtein')
+            # sim2 = self._compute_lgm_sim(a, b, 'davies')
+            # sim3 = self._compute_lgm_sim(a, b, 'skipgram')
+            # sim4 = self._compute_lgm_sim(a, b, 'soft_jaccard')
+            # sim5 = self._compute_lgm_sim(a, b, 'strike_a_match')
+            # sim6 = self._compute_lgm_sim(a, b, 'cosine')
+            # sim7 = self._compute_lgm_sim(a, b, 'jaccard')
+            # sim8 = self._compute_lgm_sim(a, b, 'monge_elkan')
+            # sim9 = self._compute_lgm_sim(a, b, 'jaro_winkler')
+            # sim10 = self._compute_lgm_sim(a, b, 'jaro')
+            # sim11 = self._compute_lgm_sim(a, b, 'jaro_winkler_r')
+            # sim12 = self._compute_lgm_sim(a, b, 'tuned_jaro_winkler')
+            # sim13 = self._compute_lgm_sim(a, b, 'tuned_jaro_winkler_r')
+            # sim14, sim15, sim16 = self._compute_lgm_sim_base_scores(a, b, 'damerau_levenshtein')
 
-        f = list(chain.from_iterable(f))
+            # f.append([
+            #     sim1, sim2, sim3, sim4, sim5, sim6, sim7, sim8, sim9, sim10, sim11, sim12, sim13, sim14, sim15, sim16
+            # ])
+
+        # f = list(chain.from_iterable(f))
 
         return f
 
@@ -181,23 +176,25 @@ class Features:
 
     @staticmethod
     def _compute_lgm_sim(s1, s2, metric, w_type='avg'):
-        baseTerms, mismatchTerms, specialTerms = lgm_sim_split(
-            s1, s2, LGMSimVars.per_metric_optValues[metric][w_type][0])
+        baseTerms, mismatchTerms, specialTerms = sim_measures.lgm_sim_split(
+            s1, s2, sim_measures.LGMSimVars.per_metric_optValues[metric][w_type][0])
 
-        if metric in ['jaro_winkler_r', 'tuned_jaro_winkler_r']:
-            return weighted_sim(
-                {'a': [x[::-1] for x in baseTerms['a']], 'b': [x[::-1] for x in baseTerms['b']],
-                 'len': baseTerms['len'], 'char_len': baseTerms['char_len']},
-                {'a': [x[::-1] for x in mismatchTerms['a']], 'b': [x[::-1] for x in mismatchTerms['b']],
-                 'len': mismatchTerms['len'], 'char_len': mismatchTerms['char_len']},
-                {'a': [x[::-1] for x in specialTerms['a']], 'b': [x[::-1] for x in specialTerms['b']],
-                 'len': specialTerms['len'], 'char_len': specialTerms['char_len']},
-                metric[:-2], True if w_type == 'avg' else False
-            )
-        else:
-            return weighted_sim(baseTerms, mismatchTerms, specialTerms, metric, True if w_type == 'avg' else False)
+        # if metric in ['jaro_winkler_r', 'tuned_jaro_winkler_r']:
+        #     return sim_measures.weighted_sim(
+        #         {'a': [x[::-1] for x in baseTerms['a']], 'b': [x[::-1] for x in baseTerms['b']],
+        #          'len': baseTerms['len'], 'char_len': baseTerms['char_len']},
+        #         {'a': [x[::-1] for x in mismatchTerms['a']], 'b': [x[::-1] for x in mismatchTerms['b']],
+        #          'len': mismatchTerms['len'], 'char_len': mismatchTerms['char_len']},
+        #         {'a': [x[::-1] for x in specialTerms['a']], 'b': [x[::-1] for x in specialTerms['b']],
+        #          'len': specialTerms['len'], 'char_len': specialTerms['char_len']},
+        #         metric[:-2], True if w_type == 'avg' else False
+        #     )
+        # else:
+        return sim_measures.weighted_sim(
+            baseTerms, mismatchTerms, specialTerms, metric, True if w_type == 'avg' else False)
 
     @staticmethod
     def _compute_lgm_sim_base_scores(s1, s2, metric, w_type='avg'):
-        base_t, mis_t, special_t = lgm_sim_split(s1, s2, LGMSimVars.per_metric_optValues[metric][w_type][0])
-        return score_per_term(base_t, mis_t, special_t, metric)
+        base_t, mis_t, special_t = sim_measures.lgm_sim_split(
+            s1, s2, sim_measures.LGMSimVars.per_metric_optValues[metric][w_type][0])
+        return sim_measures.score_per_term(base_t, mis_t, special_t, metric)
