94de7ebda5424c6403fc4c755ba56a7f426c0f8b
vkaff <vkaffes@imis.athena-innovation.gr>
2020-04-28 10:19:01 +0300

    Increased max_content_width in cli to show full help messages.

--- interlinking/cli.py
+++ interlinking/cli.py
@@ -10,13 +10,12 @@ from interlinking.sim_measures import build_dataset
 from interlinking import core
 
 
-@click.group()
+@click.group(context_settings=dict(max_content_width=120, help_option_names=['-h', '--help']))
 def cli():
     pass
 
 
-@cli.command('build', help='Tune various classifiers and select the best hyper-parameters on a '
-                                           'train dataset')
+@cli.command('build', help='\b build a candidate pairs of toponyms dataset for evaluation from Geonames')
 @click.option('--dataset', default='allCountries.txt', help='.')
 @click.option('--n_alternates', default='3', help='Min number of alternative names in order to process the record.')
 @click.option('--num_instances', default='5000000',
@@ -27,7 +26,7 @@ def build(dataset, n_alternates, num_instances, encoding):
     build_dataset(dataset, n_alternates, num_instances, encoding)
 
 
-@cli.command('extract_frequent_terms', help='Create a file with ranked frequent terms found in corpus')
+@cli.command('extract_frequent_terms', help='create a file with ranked frequent terms found in corpus')
 @click.option('--train_set', default='dataset-string-similarity_global_1k.csv',
               help='Train dataset to extract frequent terms from. '
                    'It requires once to run: python -m nltk.downloader \'punkt\'')
@@ -37,36 +36,36 @@ def freq_terms(train_set, encoding):
     pre_process.extract_freqterms(train_set, encoding)
 
 
-@cli.command('learn_sim_params', help='Learn parameters, i.e., weights/thresholds, on a train dataset for '
+@cli.command('learn_sim_params', help='learn parameters, i.e., weights/thresholds, on a train dataset for '
                                       'similarity metrics')
 @click.option('--train_set', default='dataset-string-similarity_global_1k.csv',
               help='Train dataset to learn parameters.')
 @click.option('--sim_type', default='basic', type=click.Choice(['basic', 'sorted', 'lgm']),
               help='Group of similarities to train. Valid options are: basic, sorted, lgm')
 @click.option('--encoding', default='latin', type=click.Choice(['latin', 'global']),
-              help='specify the alphabet encoding of toponyms in dataset.')
+              help='Specify the alphabet encoding of toponyms in dataset.')
 def learn_params(train_set, sim_type, encoding):
     if sim_type == 'lgm':
         learn_params_for_lgm(train_set, encoding)
     else: learn_thres(train_set, sim_type)
 
 
-@cli.command('hyperparameter_tuning', help='Tune various classifiers and select the best hyper-parameters on a '
+@cli.command('hyperparameter_tuning', help='tune various classifiers and select the best hyper-parameters on a '
                                            'train dataset')
 @click.option('--train_set', default='dataset-string-similarity_global_1k.csv',
               help='Train dataset to learn parameters.')
 @click.option('--test_set', default='dataset-string-similarity.txt', help='Test dataset to evaluate the models.')
 @click.option('--encoding', default='latin', type=click.Choice(['latin', 'global']),
-              help='specify the alphabet encoding of toponyms in dataset.')
+              help='Specify the alphabet encoding of toponyms in dataset.')
 def hyperparams_learn(train_set, test_set, encoding):
     core.StrategyEvaluator(encoding).hyperparamTuning(train_set, test_set)
 
 
-@cli.command('evaluate', help='Tune various classifiers and select the best hyper-parameters on a train dataset')
-@click.option('--train_set', default='', help='Train dataset to learn parameters.')
-@click.option('--test_set', default='dataset-string-similarity.txt', help='Test dataset to validate the models.')
+@cli.command('evaluate', help='')
+@click.option('--train_set', default='', help='the dataset to train the models.')
+@click.option('--test_set', default='dataset-string-similarity.txt', help='the dataset to apply/evaluate trained models.')
 @click.option('--encoding', default='latin', type=click.Choice(['latin', 'global']),
-              help='specify the encoding of toponyms in dataset.')
+              help='Specify the encoding of toponyms in dataset.')
 def eval_classifiers(train_set, test_set, encoding):
     core.StrategyEvaluator(encoding).evaluate(train_set, test_set)
 
